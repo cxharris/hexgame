bc174cd2734b817b68b6a75e1c1d4a3e570811c4
Clive <clive.harris@gmail.com>
2023-03-30 22:00:26 +0100

    Test mode co-ordinates displayed

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class test : MonoBehaviour
 {
+    public GameObject textPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
         foreach (Cell cell in HexMap.Instance.GetCellList())
         {
-            Debug.Log(cell.GetCoords().x + " , " + cell.GetCoords().z);
+            GameObject textObject = Instantiate(textPrefab);
+            textObject.transform.eulerAngles = new Vector3(90, 0, 0);
+            RenderLegend(cell, textObject);
         }
     }
 
@@ -17,5 +22,14 @@ public class test : MonoBehaviour
     void Update()
     {
 
+    }
+
+    private void RenderLegend(Cell cell, GameObject textObject)
+    {
+        TextMeshPro textMesh = textObject.GetComponent<TextMeshPro>();
+        textMesh.text = cell.GetCoords().x + "," + cell.GetCoords().z;
+        //textMesh.fontSize = 6;
+        //textMesh.color = Color.black;
+        textMesh.transform.position = cell.transform.position + new Vector3(0f, 0.01f, 0f);
     }
 }
