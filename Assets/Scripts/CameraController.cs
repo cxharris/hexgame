3abf52a274a9235152c01da7ac4d55da1acef7a4
Clive <clive.harris@gmail.com>
2023-04-01 21:17:00 +0100

    Got camera working

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    private const float MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float MAX_FOLLOW_Y_OFFSET = 12f;
+
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private CinemachineTransposer cinemachineTransposer;
+    private Vector3 targetFollowOffset;
+
+    private void Start()
+    {
+        cinemachineTransposer = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        targetFollowOffset = cinemachineTransposer.m_FollowOffset;
+    }
     // Update is called once per frame
     void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+        HandleZoom();
+    }
+
+    private void HandleMovement()
     {
         Vector3 inputMoveDirection = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W))
@@ -28,6 +49,10 @@ public class CameraController : MonoBehaviour
         Vector3 moveVector = transform.forward * inputMoveDirection.z + transform.right * inputMoveDirection.x;
         transform.position += moveVector * moveSpeed * Time.deltaTime;
 
+    }
+
+    private void HandleRotation()
+    {
         Vector3 rotationVector = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.Q))
         {
@@ -39,5 +64,22 @@ public class CameraController : MonoBehaviour
         }
         float rotationSpeed = 100f;
         transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
+
+    }
+
+    private void HandleZoom()
+    {
+        float zoomAmount = 1f;
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            targetFollowOffset.y -= zoomAmount;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            targetFollowOffset.y += zoomAmount;
+        }
+        float zoomSpeed = 5f;
+        targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+        cinemachineTransposer.m_FollowOffset = Vector3.Lerp(cinemachineTransposer.m_FollowOffset, targetFollowOffset, Time.deltaTime * zoomSpeed);
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # hexgame
 
 1. DONE. Display the GridPosition of each Hex whenever the Test GameObject is active.
-2. Get the camera working.
+2. DONE. Get the camera working.
+3. Put a cube in a position
+4. Be able to identify the adjacent hexes of a hex.
+5. Move a cube to an adjacent hex, preventing it from moving beyond the boundaries of the map
+6. Apply a sprite to a cube in a visually appealing way (to be discovered). The sprites may correspond to NATO designations for units etc.
+7. Make it possible to designate a hex as different terrain types.
+   
